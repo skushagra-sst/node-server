9c85f29ed7e2141a01a8f445a6257e10ceec6b03
Kushagra S <kushagra.rigel@gmail.com>
2024-07-03 05:32:13 +0000

    added put and delete for courses

--- express-class/index.js
+++ express-class/index.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(express.json());
 
+var courseCounter = 3;
+
 const courses = [
   { id: 1, name: "java" },
   { id: 2, name: "python" },
@@ -20,22 +22,38 @@ app.post("/courses", (req, res) => {
     return res.status(400).send("Course not provided");
   }
   courses.push({
-    id: courses.length + 1,
+    id: courseCounter + 1,
     course: course,
   });
+  courseCounter += 1;
   res.send(`Course received: ${course}`);
 });
 
 app.put("/courses/:id", (req, res) => {
   const id = req.params.id;
   const course = req.body.course;
-  if (!id || !course) {
+  console.log(id);
+  console.log(course);
+  if ((!id && id != 0) || !course) {
     return res.status(400).send("Incomplete request");
   }
-  let courseMod = courses[id];
+  let courseMod = courses.find((c) => c.id == id);
   if (!courseMod) return res.status(400).send("Course with ID not found");
-  else courseMod.course = course;
+  else courseMod.name = course;
   res.send(`Course id ${id}: ${course} updated`);
+});
+
+app.delete("/courses/:id", (req, res) => {
+  const id = req.params.id;
+  const course = courses.find((c) => c.id == id);
+  console.log(id);
+  console.log(course);
+  if ((!id && id !== 0) || !course) {
+    return res.status(400).send("Bad request");
+  }
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+  res.sendStatus(200);
 });
 
 app.listen(3000, () => {
