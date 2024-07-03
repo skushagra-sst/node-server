5c02721de2f6af446e7671fe1e575fa0ca7ab466
Kushagra S <kushagra.rigel@gmail.com>
2024-07-03 04:26:47 +0000

    added post call for courses

--- express-class/index.js
+++ express-class/index.js
@@ -12,4 +12,9 @@ app.get("/courses", (req, res) => {
   res.json(courses);
 });
 
+app.post("/courses", (req, res) => {
+  const courseName = req.body.courseName;
+  courses.append({id: courses.length+1, name: courseName});
+});
+
 app.listen(3000);
