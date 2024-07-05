bec52bd4afbcc612ed9b3157ed2fc3f93d486dff
Kushagra S <kushagra.rigel@gmail.com>
2024-07-05 05:34:02 +0000

    Added reading from file instead of array of objects.

--- express-class/index.js
+++ express-class/index.js
@@ -1,22 +1,41 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const fs = require("fs");
+const mongoose = require("mongoose");
+const { log } = require("console");
+
+const db = mongoose
+  .connect(
+    "mongodb+srv://kushagrarigel:vSeaO0pgqV8trzJn@cluster0.kfxlans.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  )
+  .then(() => {
+    log("Connected");
+  })
+  .catch(() => {
+    log("Failed");
+  });
 
 const app = express();
 app.use(express.json());
 
-var courseCounter = 3;
+let courseFile = JSON.parse(fs.readFileSync("courses.json", "utf-8"));
+let courseCounter = courseFile.length;
 
-const courses = [
-  { id: 1, name: "java" },
-  { id: 2, name: "python" },
-  { id: 3, name: "c++" },
-];
+const readCourses = () => {
+  return JSON.parse(fs.readFileSync("courses.json", "utf-8"));
+};
+
+const writeCourses = (courses) => {
+  fs.writeFileSync("courses.json", JSON.stringify(courses, null, 2));
+};
 
 app.get("/courses", logger, (req, res) => {
+  const courses = readCourses();
   res.json(courses);
 });
 
 app.post("/courses", logger, (req, res) => {
+  const courses = readCourses();
   const course = req.body.course;
   if (!course) {
     return res.status(400).send("Course not provided");
@@ -26,26 +45,30 @@ app.post("/courses", logger, (req, res) => {
     course: course,
   });
   courseCounter += 1;
+  writeCourses(courses);
   res.send(`Course received: ${course}`);
 });
 
 app.put("/courses/:id", (req, res) => {
-  const id = req.params.id;
+  const courses = readCourses();
+  const id = parseInt(req.params.id);
   const course = req.body.course;
   console.log(id);
   console.log(course);
-  if ((!id && id != 0) || !course) {
+  if ((!id && id !== 0) || !course) {
     return res.status(400).send("Incomplete request");
   }
-  let courseMod = courses.find((c) => c.id == id);
+  let courseMod = courses.find((c) => c.id === id);
   if (!courseMod) return res.status(400).send("Course with ID not found");
-  else courseMod.name = course;
+  else courseMod.course = course;
+  writeCourses(courses);
   res.send(`Course id ${id}: ${course} updated`);
 });
 
 app.delete("/courses/:id", (req, res) => {
-  const id = req.params.id;
-  const course = courses.find((c) => c.id == id);
+  const courses = readCourses();
+  const id = parseInt(req.params.id);
+  const course = courses.find((c) => c.id === id);
   console.log(id);
   console.log(course);
   if ((!id && id !== 0) || !course) {
@@ -53,6 +76,7 @@ app.delete("/courses/:id", (req, res) => {
   }
   const index = courses.indexOf(course);
   courses.splice(index, 1);
+  writeCourses(courses);
   res.sendStatus(200);
 });
 
